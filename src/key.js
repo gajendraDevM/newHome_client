c5c2ad38a5ef296a5b700a58c4db8348788a823b
1990minds <1990minds@gmail.com>
2021-02-06 22:48:59 +0530

    update

--- src/key.js
+++ src/key.js
@@ -1,5 +1,5 @@
-//  const BACK_END_URI = 'https://new-home-dev.herokuapp.com'
- const BACK_END_URI = 'http://localhost:5000'
+ const BACK_END_URI = 'https://pik-server.herokuapp.com'
+//  const BACK_END_URI = 'http://localhost:5000'
 
 
  export default BACK_END_URI
